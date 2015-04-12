3a41b41d64e3659ff946e5631fa5765686ba4c39
Geoff Kimball <geoff@zurb.com>
2015-04-11 17:40:13 -0700

    Add posts being fetched from /r/front

---
+++ app/routes/index.js
@@ -0,0 +1,7 @@
+export default Ember.Route.extend({
+  model: function() {
+    return Ember.$.getJSON('http://www.reddit.com/.json').then(function(data) {
+      return data.data.children;
+    })
+  }
+})
