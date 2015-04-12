1f3e96d9171ffcd1937e4093613cdef960361f87
Geoff Kimball <geoff@zurb.com>
2015-04-11 19:07:19 -0700

    Add post detail view

---
+++ app/routes/post.js
@@ -0,0 +1,11 @@
+export default Ember.Route.extend({
+  model: function(params) {
+    var url = 'http://www.reddit.com/r/'+params.subreddit+'/comments/'+params.id+'/.json';
+    return Ember.$.getJSON(url).then(function(data) {
+      return {
+        post: data[0].data.children[0].data,
+        comments: data[1].data.children
+      }
+    })
+  }
+})
